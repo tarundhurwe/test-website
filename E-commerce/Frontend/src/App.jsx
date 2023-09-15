a7a699e75a8e3a07fef307c03b6cbefbdb2645f6
harshitaHD <harshitadhurwe@gmail.com>
2023-09-15 13:39:14 +0530

    completed home page ui

--- E-commerce/Frontend/src/App.jsx
+++ E-commerce/Frontend/src/App.jsx
@@ -1,13 +1,30 @@
-import './App.css'
-import { HomePage } from './components/HomePage'
+// import './App.css'
+// import { HomePage } from './components/HomePage'
+
+// function App() {
+
+//   return (
+//     <>
+//       <HomePage />
+//     </>
+//   )
+// }
+
+// export default App
+
+// src/App.js
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import HomePage from "./components/HomePage";
 
 function App() {
-
   return (
-    <>
+    <div className="App">
+      <Header />
       <HomePage />
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- E-commerce/Frontend/src/components/HomePage.jsx
+++ E-commerce/Frontend/src/components/HomePage.jsx
@@ -1,12 +1,75 @@
-import React from 'react'
-import { NavBar } from './NavBar'
-import { Crousel } from './Crousel'
+// import React from 'react'
+// import { NavBar } from './NavBar'
+// import { Crousel } from './Crousel'
 
-export const HomePage = () => {
-    return (
-        <>
-            <NavBar />
-            <Crousel />
-        </>
-    )
-}
+// export const HomePage = () => {
+//     return (
+//         <>
+//             <NavBar />
+//             <Crousel />
+//         </>
+//     )
+// }
+// src/pages/HomePage.js
+import React from "react";
+import ProductCard from "../components/ProductCard";
+import "../App.css";
+
+const HomePage = () => {
+  const products = [
+    {
+      id: 1,
+      name: "Product 1",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      price: 19.99,
+      image: "./images/paperclips.jpg",
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      price: 19.99,
+      image: "./images/sticky_notes.jpg",
+    },
+    {
+      id: 3,
+      name: "Product 3",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      price: 19.99,
+      image: "./images/notebooks.jpg",
+    },
+    {
+      id: 4,
+      name: "Product 4",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      price: 19.99,
+      image: "./images/leather_journal.jpg",
+    },
+    {
+      id: 5,
+      name: "Product 5",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      price: 19.99,
+      image: "./images/clipboards.jpg",
+    },
+    {
+      id: 6,
+      name: "Product 6",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      price: 19.99,
+      image: "./images/pen_set.jpg",
+    },
+  ];
+
+  return (
+    <div className="home-page">
+      <div className="product-list">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
